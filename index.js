2c6a46f43dcef8a16eb9043da843e96442d110dd
Amaan Shaikh <shaikhamaannn100@gmail.com>
2025-03-21 21:36:57 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+"use strict";
+function greets(firstName) {
+    return `Hello ${firstName}`;
+}
+const greetUser = greets("Amaan");
+console.log(greetUser);
---
+++ index.ts
@@ -0,0 +1,6 @@
+function greets(firstName: String) {
+  return `Hello ${firstName}`;
+}
+
+const greetUser = greets("Amaan");
+console.log(greetUser);
